100914584e5d5a1d9a76cdbbc575e0704e56b770
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2024-01-13 08:39:47 -0300

    feat: sacao04 - POSTGRES

---
+++ PostgresSQL/secao04/secao04 - DCL - estagiario.sql
@@ -0,0 +1,9 @@
+--INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Apple', 4500, 'New York');
+--INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Microsoft', 1200, 'São Paulo');
+--INSERT INTO empresas (nome, funcionarios, cidade) VALUES ('Amazon', 1000, 'Los Angeles');
+
+SELECT * FROM empresas;
+
+--UPDATE empresas SET funcionarios = 1500 WHERE id = 3;
+
+--DELETE FROM empresas WHERE id = 2;
