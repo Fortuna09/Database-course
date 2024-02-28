30fa51e6607153a99eb5183fda2558f456a69f65
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2024-02-28 15:09:36 -0300

    feat?

---
+++ PostgresSQL/secao05/Busca comp.sql
@@ -0,0 +1,8 @@
+
+-- SELECT * FROM tipos_produto;
+
+-- SELECT * FROM produtos;
+
+SELECT p.codigo, p.descricao, p.preco, tp.descricao AS Tipo
+	FROM produtos AS p, tipos_produto AS tp
+    WHERE p.codigo_tipo = tp.codigo;
